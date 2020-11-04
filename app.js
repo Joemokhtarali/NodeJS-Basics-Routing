39b331cdf919698b04a146691a5f2963b3f2b3b2
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2020-11-04 16:37:54 -0500

    exporting modules

--- app.js
+++ app.js
@@ -1,47 +1,6 @@
 const http = require('http')
-const fs = require('fs')
+const routes = require('../routes')
 
-const server = http.createServer((req, res) => {
-    // console.log(req.url, req.method, req.headers)
-    // process.exit()
-    const url = req.url
-    const method = req.method;
-    if(url === '/'){
-        res.write('<html>')
-        res.write('<head>')
-        res.write('<title>Enter Message</title>')
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"> </input> <button type="submit>Submit</button></form></body>')
-        res.write('</head>')
-        res.write('</html>')
-        return res.end() 
-    }
-
-    if (url === '/message' && method === 'POST'){
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk)
-            
-            body.push(chunk);
-        })
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody);
-            
-        })
-        fs.writeFile('message.txt', 'DUMMY')
-        res.statusCode = 302;
-        res.setHeader('Location', '/')
-        return res.end()
-
-    }
-    res.setHeader('Content-Type', 'text/html')
-    res.write('<html>')
-    res.write('<head>')
-    res.write('<title>Hello World</title>')
-    res.write('<body><h1>My first Node writing html</h1></body>')
-    res.write('</head>')
-    res.write('</html>')
-    res.end(); 
-});
+const server = http.createServer(routes)
 
 server.listen(3000);
