203d5f25781fd100fc805d5bdac4dfc33e91ffba
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2020-11-10 16:36:51 -0500

    adding server with app and using middleware

--- app.js
+++ app.js
@@ -11,9 +11,12 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
     console.log('In another middle');
+    res.send('<h1>Hello</h1>')
 })
 // const routes = require('../routes')
 
-const server = http.createServer(app)
+// const server = http.createServer(app)
 
-server.listen(3000);
+// server.listen(3000);
+
+app.listen(3000)
