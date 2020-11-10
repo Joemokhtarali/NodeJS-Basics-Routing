3fe9cca5344e282e354baddc74dda7b38a7ed921
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2020-11-10 16:42:07 -0500

    adding routes using app.use(path, callback)

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ const express = require('express')
 
 const app = express()
 
-app.use((req, res, next) => {
-    console.log('In the middle');
-    next()
+app.use('/product', (req, res, next) => {
+    console.log('In another middle');
+    res.send('<h1>The Product page</h1>')
 })
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log('In another middle');
     res.send('<h1>Hello</h1>')
 })
+
 // const routes = require('../routes')
 
 // const server = http.createServer(app)
